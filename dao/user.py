4333796d008352827a2df412b1cba384718eab4b
Jesi <jesi.nieves@gmail.com>
2019-05-02 20:16:24 -0400

    Update user.py

--- dao/user.py
+++ dao/user.py
@@ -90,7 +90,7 @@ class UserDAO:
                      ) AS queseyo
                      GROUP BY user_id
                      ORDER BY interactions DESC
-                     LIMIT 1
+                     
                      """
         try:
             cursor.execute(query)
